02772394698669c96a38e01755ec2a2a3b55479e
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-23 01:52:03 +0700

    Добавлен хук useStaff

---
+++ src/utils/apiHooks.ts
@@ -0,0 +1,6 @@
+﻿import { $api } from "./fetchClient.ts";
+import { useQuery } from "@tanstack/react-query";
+
+export const useStaff = () => {
+  return useQuery($api.queryOptions("get", "/api/Staff/GetAll"));
+};
