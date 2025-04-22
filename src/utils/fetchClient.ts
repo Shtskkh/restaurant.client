93598cad074d9537d6b004b6a641a7411f2d8a86
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-22 13:04:21 +0700

    Добавлен клиент для запросов к API

---
+++ src/utils/fetchClient.ts
@@ -0,0 +1,12 @@
+﻿import createFetchClient from "openapi-fetch";
+import type { paths } from "./schema";
+import createClient from "openapi-react-query";
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
+
+const fetchClient = createFetchClient<paths>({
+  baseUrl: "http://localhost:7280/",
+});
+
+export const $api = createClient(fetchClient);
