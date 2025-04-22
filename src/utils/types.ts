e0b2d2653ccdd6153765c824f12597b9dd0d453b
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-22 13:00:37 +0700

    Добавлены типы из схемы OpenAPI

---
+++ src/utils/types.ts
@@ -0,0 +1,7 @@
+﻿import { components } from "./schema";
+
+export type Staff = components["schemas"]["StaffModel"];
+export type Dish = components["schemas"]["DishModel"];
+export type ProductInDish = components["schemas"]["ProductInDishModel"];
+export type Order = components["schemas"]["OrderModel"];
+export type DishInOrder = components["schemas"]["DishInOrderModel"];
