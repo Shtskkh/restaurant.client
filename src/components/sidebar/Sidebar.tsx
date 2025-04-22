7f2df0647fd59e3e61b075296b4a5168b26f4f97
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-22 14:15:18 +0700

    Реализован базовый Sidebar

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+﻿import { Divider, Drawer, List } from "@mui/material";
+import { tabs } from "./tabs.tsx";
+import { ListItemLink } from "./ListItemLink.tsx";
+
+export const Sidebar = () => {
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: 240,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: 240,
+          boxSizing: "border-box",
+        },
+      }}
+    >
+      <List>
+        {tabs.map((tab) => (
+          <ListItemLink
+            key={tab.to}
+            to={tab.to}
+            text={tab.text}
+            icon={tab.icon}
+          />
+        ))}
+        <Divider />
+      </List>
+    </Drawer>
+  );
+};
