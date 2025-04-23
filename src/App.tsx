3cf6374b2a0575097ea5e0d7718223bbec777972
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-23 14:10:33 +0700

    Исправлен импорт

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { CssBaseline } from "@mui/material";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./utils/router.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./utils/fetchClient.ts";
+import { queryClient } from "./utils/clients.ts";
 
 function App() {
   return (
