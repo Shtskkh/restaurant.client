843d5783fe25006459d3ce3a2830e8aaff489526
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-23 14:12:11 +0700

    Убрана обработка ошибок, будет добавлена позже

--- src/components/errorComponent.tsx
+++ src/components/errorComponent.tsx
@@ -1,14 +1,9 @@
 ﻿import { Typography } from "@mui/material";
-import { DefaultError } from "@tanstack/react-query";
 
-type ErrorComponentProps = {
-  error: DefaultError;
-};
-
-const ErrorComponent = ({ error }: ErrorComponentProps) => {
+const ErrorComponent = () => {
   return (
     <Typography variant="body1" color="textPrimary" mt={3} mb={3}>
-      {error.message}
+      Что-то пошло не так!
     </Typography>
   );
 };
