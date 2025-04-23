e3eab5582412eb699e0dd2ca40437c807b310424
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-23 14:10:13 +0700

    Добавлен тип для ошибок

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,3 +5,4 @@ export type Dish = components["schemas"]["DishModel"];
 export type ProductInDish = components["schemas"]["ProductInDishModel"];
 export type Order = components["schemas"]["OrderModel"];
 export type DishInOrder = components["schemas"]["DishInOrderModel"];
+export type RequestError = components["schemas"]["ProblemDetails"];
