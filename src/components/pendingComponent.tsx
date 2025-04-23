2b8ac15a5cb1db731bdb7be321a81cc7c94e0506
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-23 20:49:08 +0700

    Убран лишний Container

--- src/components/pendingComponent.tsx
+++ src/components/pendingComponent.tsx
@@ -1,18 +1,16 @@
-﻿import { Box, CircularProgress, Container } from "@mui/material";
+﻿import { Box, CircularProgress } from "@mui/material";
 
 export const PendingComponent = () => {
   return (
-    <Container maxWidth="sm">
-      <Box
-        sx={{
-          height: "100vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <CircularProgress />
-      </Box>
-    </Container>
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress />
+    </Box>
   );
 };
