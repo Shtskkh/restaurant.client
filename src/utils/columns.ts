83563afdfeb13393071e647b2f27844d7ee96f9a
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-23 14:31:06 +0700

    Добавлены колонки для заказов

--- src/utils/columns.ts
+++ src/utils/columns.ts
@@ -1,5 +1,5 @@
 ﻿import { GridColDef } from "@mui/x-data-grid";
-import { Staff } from "./types.ts";
+import { Order, Staff } from "./types.ts";
 
 export const staffColumns: GridColDef<Staff>[] = [
   {
@@ -30,6 +30,34 @@ export const staffColumns: GridColDef<Staff>[] = [
   {
     field: "phoneNumber",
     headerName: "Номер телефона",
+    flex: 1,
+  },
+];
+
+export const ordersColumns: GridColDef<Order>[] = [
+  {
+    field: "idOrder",
+    headerName: "ID",
+    flex: 0.25,
+  },
+  {
+    field: "date",
+    headerName: "Дата",
+    flex: 1,
+  },
+  {
+    field: "status",
+    headerName: "Статус",
+    flex: 1,
+  },
+  {
+    field: "tableNumber",
+    headerName: "Столик",
+    flex: 1,
+  },
+  {
+    field: "employee",
+    headerName: "Официант",
     flex: 1,
   },
 ];
