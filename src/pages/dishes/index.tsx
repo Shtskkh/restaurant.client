27fbd4e59d947c8c30e46a128028ee09ea58154b
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-23 15:45:50 +0700

    Добавлена страница всех блюд

--- src/pages/dishes/index.tsx
+++ src/pages/dishes/index.tsx
@@ -1,10 +1,28 @@
-﻿import { Typography } from "@mui/material";
+﻿import { useDishes } from "../../utils/apiHooks.ts";
+import { PendingComponent } from "../../components/pendingComponent.tsx";
+import ErrorComponent from "../../components/errorComponent.tsx";
+import { DataGrid } from "@mui/x-data-grid";
+import { dishesColumns } from "../../utils/columns.ts";
 
 const DishesIndex = () => {
+  const { data, error, isLoading } = useDishes();
+
+  if (isLoading) {
+    return <PendingComponent />;
+  }
+
+  if (!data || error) {
+    return <ErrorComponent />;
+  }
+
   return (
-    <Typography variant="body1" color="textPrimary" mt={3} mb={3}>
-      Внутренний контент страницы
-    </Typography>
+    <DataGrid
+      columns={dishesColumns}
+      rows={data}
+      getRowId={(row) => row.idDish}
+      disableColumnMenu={true}
+      hideFooter={true}
+    />
   );
 };
 
