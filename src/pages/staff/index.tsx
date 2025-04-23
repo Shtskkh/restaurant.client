22bc8adc0ff60ec9ab8778747d961098ea335ea3
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-23 14:20:42 +0700

    Добавлен переход на профиль сотрудника

--- src/pages/staff/index.tsx
+++ src/pages/staff/index.tsx
@@ -1,19 +1,26 @@
 ﻿import ErrorComponent from "../../components/errorComponent.tsx";
 import { useStaff } from "../../utils/apiHooks.ts";
 import { PendingComponent } from "../../components/pendingComponent.tsx";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridRowParams } from "@mui/x-data-grid";
 import { staffColumns } from "../../utils/columns.ts";
+import { useNavigate } from "@tanstack/react-router";
 
 const StaffIndex = () => {
   const { data, error, isLoading } = useStaff();
+  const navigate = useNavigate();
 
   if (isLoading) {
     return <PendingComponent />;
   }
 
   if (error) {
-    return <ErrorComponent error={error} />;
+    return <ErrorComponent />;
   }
+
+  const handleClick = (gridParams: GridRowParams): void => {
+    const id: string = gridParams.row.idEmployee;
+    navigate({ to: "/staff/$id", params: { id } }).then();
+  };
 
   return (
     <DataGrid
@@ -21,6 +28,7 @@ const StaffIndex = () => {
       rows={data}
       getRowId={(row) => row.idEmployee}
       disableColumnMenu={true}
+      onRowDoubleClick={handleClick}
       hideFooter={true}
     />
   );
