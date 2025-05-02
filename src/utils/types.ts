c0cd699f723c4eac0086f572fa30d6b821c493fb
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-02 20:56:33 +0700

    Добавлен тип для поставок

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,3 +6,4 @@ export type ProductInDish = components["schemas"]["ProductInDishModel"];
 export type Order = components["schemas"]["OrderModel"];
 export type DishInOrder = components["schemas"]["DishInOrderModel"];
 export type RequestError = components["schemas"]["ProblemDetails"];
+export type Supply = components["schemas"]["SupplyModel"];
