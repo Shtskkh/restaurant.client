69d6b425e7a4adb75f078a0be1a61719ac259ae4
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-03 11:15:18 +0700

    Добавлен dialog для добавления нового сотрудника

--- src/pages/staff/index.tsx
+++ src/pages/staff/index.tsx
@@ -4,9 +4,20 @@ import { PendingComponent } from "../../components/pendingComponent.tsx";
 import { DataGrid, GridRowParams } from "@mui/x-data-grid";
 import { staffColumns } from "../../utils/columns.ts";
 import { useNavigate } from "@tanstack/react-router";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 
 const StaffIndex = () => {
   const { data, error, isLoading } = useStaff();
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
 
   if (isLoading) {
@@ -17,20 +28,52 @@ const StaffIndex = () => {
     return <ErrorComponent />;
   }
 
-  const handleClick = (gridParams: GridRowParams): void => {
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleRowDoubleClick = (gridParams: GridRowParams): void => {
     const id: string = gridParams.row.idEmployee;
     navigate({ to: "/staff/$id", params: { id } }).then();
   };
 
   return (
-    <DataGrid
-      columns={staffColumns}
-      rows={data}
-      getRowId={(row) => row.idEmployee}
-      disableColumnMenu={true}
-      onRowDoubleClick={handleClick}
-      hideFooter={true}
-    />
+    <>
+      <Button variant="contained" sx={{ marginBottom: 4 }} onClick={handleOpen}>
+        Добавить сотрудника
+      </Button>
+      <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+        <DialogTitle align="center">Новый сотрудник</DialogTitle>
+        <DialogContent>
+          <DialogContentText sx={{ mb: 1 }}>
+            Заполните форму нового сотрудника
+          </DialogContentText>
+          <TextField required label="Фамилия" margin="dense" fullWidth />
+          <TextField required label="Имя" margin="dense" fullWidth />
+          <TextField label="Отчество" margin="dense" fullWidth />
+          <TextField required label="Должность" margin="dense" fullWidth />
+          <TextField required label="Номер телефона" margin="dense" fullWidth />
+          <TextField required label="Логин" margin="dense" fullWidth />
+          <TextField required label="Пароль" margin="dense" fullWidth />
+          <DialogActions sx={{ mt: 2 }}>
+            <Button onClick={handleClose}>Отмена</Button>
+            <Button type="submit">Добавить</Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+      <DataGrid
+        columns={staffColumns}
+        rows={data}
+        getRowId={(row) => row.idEmployee}
+        disableColumnMenu={true}
+        onRowDoubleClick={handleRowDoubleClick}
+        hideFooter={true}
+      />
+    </>
   );
 };
 
